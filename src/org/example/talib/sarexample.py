5443e425e856ed23c0109350e54eeb183b9fa42d
shakti_sd <shakti.sd@gmail.com>
2012-12-01 08:09:30 +0530

    Updated SAR with buy/sell signals

--- src/org/example/talib/sarexample.py
+++ src/org/example/talib/sarexample.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
 from matplotlib.finance import candlestick
 
+
 # Download sample data
 spy = web.get_data_yahoo('SPY', '2010-01-01')
 
@@ -27,15 +28,33 @@ spy_ochl = np.array(pd.DataFrame({'0':date2num(spy.index),
 
 analysis = pd.DataFrame(index = spy.index)
 analysis['SAR'] = ta.SAR(spy.High,spy.Low, acceleration=0.05, maximum=0.2)
+analysis['Current'] = spy.Close
+
+# Record signals
+analysis['signal_bull'] = np.where((analysis.Current > analysis.SAR) &
+                                   (analysis.Current.shift(1) < analysis.SAR.shift(1)) , 1, 0)
+
+analysis['signal_bear'] = np.where((analysis.Current < analysis.SAR) &
+                                   (analysis.Current.shift(1) > analysis.SAR.shift(1)) , 1, 0)
+
 # Prepare plot
 fig, ax = plt.subplots(1, 1, sharex=True)
 ax.set_ylabel('SP500', size=20)
-
 # Plot candles
 candlestick(ax, spy_ochl, width=0.5, colorup='g', colordown='r', alpha=1)
-
 # Draw Moving Averages
 analysis.SAR.plot(c='r')
+
+# Plot entry arrows
+for i in analysis.index: 
+    if analysis.signal_bull[i]:
+        plt.arrow(date2num(i), spy.High[i], 0, 2,
+                  fc="b", ec="g", head_width=5, head_length=2)
+
+    if analysis.signal_bear[i]:
+        plt.arrow(date2num(i), spy.Low[i], 0, -2,
+                  fc="r", ec="r", head_width=5, head_length=2)
+
 
 # Show the picture!
 plt.show()
